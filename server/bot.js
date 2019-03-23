0f36d367b88b7b40a4e17a6a809691b07ffe2d2d
entrptaher@gmail.com <entrptaher@gmail.com>
2019-03-23 16:09:41 +0600

    sets targets for bot

--- server/bot.js
+++ server/bot.js
@@ -1,12 +1,31 @@
 class Bot{
-  constructor(){}
-  init(){}
-  fillForm(){}
-  submitForm(){}
-  getTitle(){}
-  getCookies(){}
-  clickBtn(){}
-  logOut(){}
+  constructor(){
+    // sets cookies, proxies, various navigation options etc for later usage
+  }
+  init(){
+    // create a browser instance
+    // create a page
+    // setup all useragent, proxy etc required
+  }
+  navigate(){
+    // loads a page
+  }
+  fillForm(){
+    // waits for element
+    // fill the element with required data
+  }
+  clickBtn(){
+    // clicks specific button, simulated/triggers a mousedown/keydown/click event
+  }
+  getText(){
+    // gets text for certain selector on the page
+  }
+  getTitle(){
+    // get current page title
+  }
+  getCookies(){
+    // get current page cookies
+  }
 }
 
 module.exports = Bot;
